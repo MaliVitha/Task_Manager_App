1f206dd5abb485adf56490bde6ac964d4263e976
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-07-08 18:36:02 +0530

    Implementation Adding functions

--- to-do-app/src/components/InputBox/InputBox.js
+++ to-do-app/src/components/InputBox/InputBox.js
@@ -2,10 +2,19 @@ import React from "react";
 import style from './InputBox.module.css';
 
 const InputBox = () => {
+
+  const clickHandler = () => {
+    console.log('Clicked !');
+  }
+
+  const typeHandler = (event) => {
+
+  }
+
   return (
     <div className={style.outer_div}>
-      <input className={style.input_style}  type='text' placeholder='Input Text' />
-      <button className={style.button_style} type='button' > ADD </button>
+      <input onChange={typeHandler} className={style.input_style}  type='text' placeholder='Input Text' />
+      <button className={style.button_style} type='button' onClick={clickHandler} > ADD </button>
     </div>
   );
 }
